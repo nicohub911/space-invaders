6df28af37fdce21313d6ed0826cf36a7b2c95cfb
nicolas <nicolaser911@gmail.com>
2024-08-09 17:14:47 -0300

    update package json

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://nicohub911.github.io/space-invaders/',
 })
